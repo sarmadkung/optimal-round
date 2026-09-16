16bc8d50788f220e16ba6605e0c753570e58d10d
Muhammad Sarmad <muhammadsarmad24@gmail.com>
2026-09-16 17:00:24 +0500

    feat: implement reverseString function using two pointers to reverse a given string

--- problems/02-two-pointers/174-reverse-string.js
+++ problems/02-two-pointers/174-reverse-string.js
@@ -27,7 +27,17 @@
  */
 
 function reverseString(s) {
-  // TODO: your solution here
+  let left = 0;
+  let right = s.length-1;
+
+  while(left<right){
+    let temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+    left++;
+    right--;
+  }
+  return s;
 }
 
 module.exports = { reverseString };
